adeb0392bdea9db6d46eeaad6bc9df092d6211b6
William Dellinger <wedellinger@gmail.com>
2021-02-14 21:13:15 -0700

    Display Board function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -6,20 +6,27 @@ std::vector<char> createBoard(){
 
 
     for(int i = 0; i < 9; i++){
-        board.push_back('0');
+        board.push_back('*');
     }
     return board;
+}
+
+void DisplayBoard(std::vector<char> myBoard){
+    int counter = 0;
+    for(int i = 0; i <= 9; i++){
+        std::cout << myBoard[i];
+        counter++;
+        if(counter == 3){
+            counter = 0;
+            std::cout << '\n';
+        }
+    }
 }
 
 int main(){
 
     std::vector<char> myBoard = createBoard();
-
-    for(int i = 0; i <= 9; i++){
-        std::cout << myBoard[i];
-        if((i+1) % 3 == 0){
-            std::cout << '\n';
-        }
-    }
+    DisplayBoard(myBoard);
+    
 
 }
