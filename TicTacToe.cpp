80a4d6e945aa7cac45951ca2014f5258aba69ab0
Jaclyn Stickrod <mail4jaci@gmail.com>
2021-02-14 21:10:37 -0700

    Adding DisplayBoard function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -11,15 +11,28 @@ std::vector<char> createBoard(){
     return board;
 }
 
+void DisplayBoard(std::vector<char> board){
+    for(int i = 0; i <= 9; i++){
+        std::cout << board[i] << "|";
+        if((i+1) % 3 == 0){
+            std::cout << '\n';
+            std::cout << "------ \n";
+            
+        }
+    }
+}
+
 int main(){
 
     std::vector<char> myBoard = createBoard();
 
-    for(int i = 0; i <= 9; i++){
-        std::cout << myBoard[i];
-        if((i+1) % 3 == 0){
-            std::cout << '\n';
-        }
-    }
+    DisplayBoard(myBoard);
+
+    // for(int i = 0; i <= 9; i++){
+    //     std::cout << myBoard[i];
+    //     if((i+1) % 3 == 0){
+    //         std::cout << '\n';
+    //     }
+    // }
 
 }
